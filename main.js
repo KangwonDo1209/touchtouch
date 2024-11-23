a82fd1cff2a2847a67a0729ee3e64c75678a87ad
KangwonDo1209 <ehtjd1209@gmail.com>
2024-11-23 20:47:30 +0900

    count test

--- main.js
+++ main.js
@@ -1,15 +1,20 @@
 const express = require("express");
 const path = require("path");
+const fs = require("fs");
 const app = express();
 const port = 3000;
 
+let count = 0;
+
+// 정적 파일 경로 설정
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
-    //res.send("Hello World!");
-    res.sendFile(path.join(__dirname, "public", "index.html"));
+    console.log(`count is ${++count}`);
+    res.sendFile(path.join(__dirname, "public", "home.html"));
 });
 
 app.listen(port, () => {
     console.log(`My App listening on port ${port}`);
+    count = 0;
 });
